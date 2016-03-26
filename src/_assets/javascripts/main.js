1a04054ccf1edb2a0f6ab912285a94b312dcd344
Jon Burr <jonburr@Jons-MacBook-Air.local>
2016-03-25 19:56:40 -0700

    build out new homepage, add prettify js and css, add dartlang.html page template and homepage.html layout

--- src/_assets/javascripts/main.js
+++ src/_assets/javascripts/main.js
@@ -1,3 +1,14 @@
 // $(function() {
 //     alert("HELLO");
 // });
+$(document).on('ready', function(){
+  prettyPrint();
+  $('.highlight').mouseenter(function(){
+    var text = $(this).data('text');
+    $('#code-display p').text(text);
+    window.console.log(text);
+  });
+  $('.highlight').mouseout(function(){
+    $('#code-display p').text('Hover over code snippet on the left to learn more.');
+  });
+});
