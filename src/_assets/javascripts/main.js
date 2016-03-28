50192428b102486042ef8cd18b41a332cd491036
Jon Burr <jonburr@Jons-MacBook-Air.local>
2016-03-27 19:38:03 -0700

    add js for internal hash scrolling effect (not complete)

--- src/_assets/javascripts/main.js
+++ src/_assets/javascripts/main.js
@@ -1,6 +1,24 @@
 // $(function() {
 //     alert("HELLO");
 // });
+
+// scroll effect
+$('a[href*="#"]:not([href="#"])').click(function(e) {
+  e.preventDefault();
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+    || location.hostname == this.hostname) {
+
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+     if (target.length) {
+       $('html,body').animate({
+           scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+
 $(document).on('ready', function(){
   prettyPrint();
   $('.highlight').mouseenter(function(){
@@ -8,7 +26,7 @@ $(document).on('ready', function(){
     $('#code-display p').text(text);
     window.console.log(text);
   });
-  $('.highlight').mouseout(function(){
+  $('.highlight').mouseleave(function(){
     $('#code-display p').text('Hover over code snippet on the left to learn more.');
   });
 });
