b1a9daebe5a41744d338f65d3055c6ff6732dd26
Tom Beck <tom@tastylounge.com>
2016-05-25 14:29:09 -0700

    match updates to dartlang

--- src/_assets/javascripts/main.js
+++ src/_assets/javascripts/main.js
@@ -1,18 +1,23 @@
 //= require vendor/jquery-1.12.3.min
 //= require bootstrap
+//= require _utilities
 //= require vendor/code-prettify/prettify
 //= require vendor/code-prettify/lang-dart
 //= require vendor/code-prettify/lang-yaml
 
+function fixNav() {
+  var t = $(document).scrollTop(),
+      f = $("#page-footer").offset().top,
+      h = window.innerHeight,
+      mh = f - t - 90;
+  $("#sidenav, #toc").css({maxHeight: mh});
+}
 
 // Add scroll on page load for hash
 $(window).on('load', function (e){
-  // window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
   if (window.location.hash) {
-    var offset = 70;
-    if ($('body').hasClass('show_subnav')) { offset = 102; }
-
-    $('html, body').animate({ scrollTop: $(window.location.hash).offset().top-offset }, 500, function (){
+    $('html, body').animate({ scrollTop: $(window.location.hash).offset().top-70 }, 500, function (){
       // Mark as active
         $('a[href^="#"]').parent('li').removeClass('active');
         $('a[href="'+window.location.hash+'"]').parent('li').addClass('active');
@@ -22,8 +27,8 @@ $(window).on('load', function (e){
 
 // When a user scrolls to 50px add class  condensed-header to body
 $(window).scroll(function(){
-  var currentScreenPosition  = $(document).scrollTop();
-
+  fixNav();
+  var currentScreenPosition = $(document).scrollTop();
   if(currentScreenPosition > 50) {
     $('body').addClass('condensed_header');
   } else {
@@ -43,8 +48,8 @@ $(document).on('ready', function(){
     $('#code-display p').text('Hover over code snippet on the left to learn more.');
   });
 
-  // Sidebar nav
-  $('#sidebar i').on('click', function(e) {
+  // Sidenav
+  $('#sidenav i').on('click', function(e) {
     window.console.log("CLICKED");
     e.preventDefault();
     $(this).parent('li').toggleClass('active');
@@ -53,48 +58,46 @@ $(document).on('ready', function(){
   // TOC: Table of Contents
   $('.toc-entry').not('.toc-h2').remove();
   $('.section-nav').addClass('nav').css({opacity: 1});
+
   $('body').scrollspy({
      offset: 100,
      target: '#toc'
   });
+
   $('#toc').on('activate.bs.scrollspy', function () {
     // do something…
   });
-  // $('#toc i').on('click', function(e) {
-  //   window.console.log("CLICKED");
-  //   e.preventDefault();
-  //   $(this).parent('li').toggleClass('active');
-  // });
 
-  // TOC: Add scroll animations
   $('#toc a[href^="#"]').click(function() {
-      var target = $(this.hash),
-          hash = this.hash,
-          offset = 70;
-      if ($('body').hasClass('show_subnav')) { offset = 102; }
-      if (target.length == 0) { target = $('a[name="' + this.hash.substr(1) + '"]'); }
-      if (target.length == 0) { target = $('html'); }
-      $('html, body').animate({ scrollTop: target.offset().top-offset }, 500, function (){
+      var target = $(this.hash);
+      var hash = this.hash;
+      if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
+      if (target.length == 0) target = $('html');
+      $('html, body').animate({ scrollTop: target.offset().top-70 }, 500, function (){
           location.hash = hash;
-          // Mark as active
-          $('a[href^="#"]').parent('li').removeClass('active');
-          $(this).parent('li').addClass('active');
       });
+      // Mark as active
+      // $('a[href^="#"]').parent('li').removeClass('active');
+      $(this).parent('li').addClass('active');
       return false;
   });
-
-  // Initiate Popovers
+  
+  // Popovers
   $('[data-toggle="popover"], .dart-popover').popover()
 
   // open - close mobile navigation
-  $('.menu-toggle').on('click', function(e) {
+  $('#menu-toggle').on('click', function(e) {
     e.stopPropagation();
     $("body").toggleClass('open-menu');
   });
+
   $("#page-content").on('click', function() {
     if ($('body').hasClass('open-menu')) {
       $('body').removeClass("open-menu");
     }
   });
 
+  $(window).smartresize(fixNav());
+
 });
+
--- src/index.md
+++ src/index.md
@@ -3,7 +3,6 @@ layout: homepage
 reference: new
 title: "Dart for the Web"
 ---
-
 paragraph Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis distinctio ducimus neque harum maxime corrupti libero accusamus, voluptates ad facere quod cupiditate tenetur, saepe possimus maiores? Ut, fuga ullam hic.
 
 ## Get Dart
